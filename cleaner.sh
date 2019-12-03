b4e5fc505abc5bd5e4a1e8232d87fc1bbe1d97da
BooDeer <hboudhir@e2r6p8.1337.ma>
2019-12-03 21:18:08 +0100

    cleaner.sh

--- cleaner.sh
+++ cleaner.sh
@@ -6,7 +6,7 @@
 #    By: hboudhir <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/12/01 16:42:36 by hboudhir          #+#    #+#              #
-#    Updated: 2019/12/03 20:34:25 by hboudhir         ###   ########.fr        #
+#    Updated: 2019/12/03 21:15:17 by hboudhir         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,10 +14,13 @@
 n=`echo $'\n.'`
 n=${n%.}
 
+echo "Your actual storage is :$n"
+df -h | tail -n 1 && sleep 2 && echo "$n"
 echo "Cleaning the cache . . . . . ."
 read -p "          	   . . . . . .$n" -t 1
 read -p "          	   . . . . . .$n" -t 1
 read -p "          	   . . . . . .$n" -t 1
 rm -rf ~/Library/*42_cache*
 rm -rf ~/.*coco*
-echo "Cache cleaned!"
+clear && echo "Cache cleaned!$n Your actual storage is :$n"
+df -h | tail -n 1 && echo "$n"
