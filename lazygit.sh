578b5f16c28dd5063feb21a32a89915c53630dcc
BooDeer <hsissi3a@gmail.com>
2019-12-04 17:08:55 +0100

    lazygit.sh

---
+++ lazygit.sh
@@ -0,0 +1,23 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    lazygit.sh                                         :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: hboudhir <marvin@42.fr>                    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2019/12/04 16:57:33 by hboudhir          #+#    #+#              #
+#    Updated: 2019/12/04 17:08:10 by hboudhir         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+n=`echo $'\n'`
+
+echo "Would you like to add or to remove files?$n"
+read	ch1
+echo "Which files would you like to $ch1$n"
+read	ch2
+echo "Insert your commit :$n"
+read	ch3
+git $ch1 $ch2 && git commit -m "$ch3" && git push
+
+
