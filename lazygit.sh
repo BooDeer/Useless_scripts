7f140b2df313f3ef6b9f1480f01c268b2c25fdcf
BooDeer <hsissi3a@gmail.com>
2019-12-06 15:18:31 +0100

    lazygit.sh

--- lazygit.sh
+++ lazygit.sh
@@ -9,13 +9,13 @@
 #    Updated: 2019/12/04 17:08:10 by hboudhir         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
+#!/bin/bash
 
 n=`echo $'\n'`
-
 echo "Would you like to add or to remove files?$n"
 read	ch1
-echo "Which files would you like to $ch1$n"
-read	ch2
+echo "Which files would you like to "$ch1"$n"
+read -e ch2
 echo "Insert your commit :$n"
 read	ch3
 git $ch1 $ch2 && git commit -m "$ch3" && git push
