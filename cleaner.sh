434627223b0f6fbf9072c2a247b745278c538ba4
BooDeer <hsissi3a@gmail.com>
2020-01-11 16:39:37 +0100

    cleaner.sh

--- cleaner.sh
+++ cleaner.sh
@@ -6,7 +6,7 @@
 #    By: hboudhir <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/12/01 16:42:36 by hboudhir          #+#    #+#              #
-#    Updated: 2019/12/11 14:03:12 by hboudhir         ###   ########.fr        #
+#    Updated: 2020/01/11 16:39:07 by hboudhir         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -15,11 +15,11 @@ n=`echo $'\n.'`
 n=${n%.}
 
 echo "Your actual storage is :$n"
-df -h | tail -n 1 && sleep 2 && echo "$n"
+df -h | grep "Users" && sleep 2&& echo "$n"
 echo "Cleaning the cache . . . . . ."
 read -p "          	   . . . . . .$n" -t 1
 read -p "          	   . . . . . .$n" -t 1
 read -p "          	   . . . . . .$n" -t 1
 rm -rf ~/Library/*42_cache* && rm -rf ~/.*coco* && brew cleanup
 clear && echo "Cache cleaned!$n Your actual storage is :$n"
-df -h | tail -n 1 && echo "$n"
+df -h | grep "Users" && echo "$n"
