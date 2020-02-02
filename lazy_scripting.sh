0f1af3302c2b27b4d4b8c43fb882b99cea75ec85
BooDeer <hsissi3a@gmail.com>
2020-02-02 06:24:49 +0100

    lazy_scripting.sh

--- lazy_scripting.sh
+++ lazy_scripting.sh
@@ -6,14 +6,15 @@
 #    By: hboudhir <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/01/27 22:48:28 by hboudhir          #+#    #+#              #
-#    Updated: 2020/01/28 00:22:44 by hboudhir         ###   ########.fr        #
+#    Updated: 2020/02/02 06:23:13 by hboudhir         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/bash
 
-CACHE=$(df -h | grep "Users" | rev | cut -d ' ' -f 18 | rev | cut -c1-1)
-if [ $CACHE -gt 3 ]
+CACHE=$(df -h | grep "Users" |  awk '{printf $3}' | cut -c1-1)
+if [ "$CACHE" -gt 3 ]
 then
 	~/Useless_scripts/cleaner.sh
 fi
+
