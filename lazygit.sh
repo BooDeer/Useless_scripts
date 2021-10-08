1aefbd35952db0a7f564556dda57ec0446b22b1f
BooDeer <hsissi3a@gmail.com>
2021-10-08 17:33:09 +0100

    Added some colors + fixed typo

--- lazygit.sh
+++ lazygit.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+CYAN_COLOR='\033[1;36m'
+RED_COLOR='\033[0;31m'
+DEFAULT_COLOR='\033[0m'
 
-echo -n "Would you like to (a)dd or to (r)emove files? "
+printf "${CYAN_COLOR}Would you like to (a)dd or to (r)emove files: ${DEFAULT_COLOR}"
 read	ch1
 case	$ch1 in
 	a|add)
@@ -10,13 +13,13 @@ case	$ch1 in
 		ch1='rm'
 		;;
 	*)
-		printf "'$ch1' is an invalid option!\nUsage:  a: Adds a file to the remote verison.\n \tr: Deletes a file in the remote version.\n"
+		printf "${RED_COLOR}'$ch1' is an invalid option!\n${DEFAULT_COLOR}Usage:  a: Adds a file to the remote version.\n \tr: Deletes a file in the remote version.\n"
 		exit 1
 		;;
 esac
-echo -n "Which files would you like to $ch1 : "
+printf "${CYAN_COLOR}Which files would you like to $ch1: ${DEFAULT_COLOR}"
 read -e ch2
-echo -n "Insert your commit : "
+printf "${CYAN_COLOR}Insert your commit: ${DEFAULT_COLOR}"
 read	ch3
 git $ch1 $ch2 && git commit -m "$ch3" && git push
 
